abd079a08b946b6f78e7d30836a1acecb6bf8902
Scott Garrett <Wintervenom@archlinux.us>
2012-07-01 00:22:28 -0500

    [Auto Update] (No comment)

--- wm/openbox/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DESKTOP_ENV=""
-if which /usr/lib/openbox/xdg-autostart >/dev/null; then
-    /usr/lib/openbox/xdg-autostart $DESKTOP_ENV
-fi
-reset-tint2 &
-feh --bg-fill ~/.background
